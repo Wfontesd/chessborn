3de458a0e2010fa00e3dd9bf3ce2226e7b92bc96
Chessborn web publisher <pages@chessborn.invalid>
2026-08-04 04:39:35 +0000

    Publish web build for 7742f517878c59315464a6c5ee95267c6110802d

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785815897|10621275';
+const CACHE_VERSION = '1785818371|7742409';
 /** @type {string} */
 const CACHE_PREFIX = 'Chessborn-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
